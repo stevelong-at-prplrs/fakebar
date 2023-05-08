9798aaa44b2e2bfd6a35803e0d34f6d0ffa7645b
Steve Long <stevelong@prpl.rs>
2023-05-08 12:29:38 -0500

    cleanup

--- setup.sh
+++
@@ -1,4 +0,0 @@
-echo setup script
-mkdir docs/react
-cp ./node_modules/react/umd/react.development.js ./docs/react/react.development.js
-cp ./node_modules/react-dom/umd/react-dom.development.js ./docs/react/react-dom.development.js
